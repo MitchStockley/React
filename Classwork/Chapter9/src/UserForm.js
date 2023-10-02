d2f141887c63bed50b64dd282dabb7de364c967d
Mitchell <mitchell.stockley@gmail.com>
2023-10-02 11:45:54 +0200

    commented userform.js

--- Classwork/Chapter9/src/UserForm.js
+++ Classwork/Chapter9/src/UserForm.js
@@ -1,32 +1,43 @@
-// import React, { Component } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import firebase from 'firebase/compat/app';
+// Import necessary modules and components
+import React, { Component } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik'; // Formik for form management
+import firebase from 'firebase/compat/app'; // Firebase for database operations
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/database';
-import { useParams } from 'react-router-dom';
-import { Component } from 'react';
+import { useParams } from 'react-router-dom'; // useParams for getting route parameters
 
+// Define a class-based component called UserForm
 class UserForm extends Component {
 
+    // Declare class properties for title and id
     title;
     id;
 
+    // Constructor for the component
     constructor(props) {
         super(props);
+
+        // Initialize id from route parameters and set a default title
         this.id = this.props.params.id;
         this.title = 'New User';
+
+        // Initialize component state for username and email
         this.state = {
             username: '',
             email: '',
         };
+
+        // If id is present (editing an existing user), update the title
         if (this.id) {
             this.title = 'Edit User';
         }
     }
 
+    // Lifecycle method that runs after the component is mounted
     componentDidMount() {
         if (this.id) {
+            // Fetch user data from Firebase and update component state
             firebase.database().ref('/' + this.id)
                 .on('value', snapshot => {
                     this.setState({
@@ -37,6 +48,7 @@ class UserForm extends Component {
         }
     }
 
+    // Render method for the component
     render() {
         return (
             <div>
@@ -48,6 +60,7 @@ class UserForm extends Component {
                         email: this.state.email
                     }}
                     validate={values => {
+                        // Validation logic for username and email fields
                         let errors = {};
                         if (!values.email) {
                             errors.email = 'Required';
@@ -64,27 +77,30 @@ class UserForm extends Component {
                             errors.username = 'Required';
                         }
                         else if (values.username.length < 3) {
-                            errors.username = 'username too short';
+                            errors.username = 'Username too short';
                         }
                         return errors;
                     }}
 
                     onSubmit={(values, { setSubmitting }) => {
+                        // Form submission logic
                         setTimeout(() => {
-                            // actual submit logic... 
                             if (this.id) {
+                                // Update existing user data in Firebase
                                 firebase.database().ref('/' + this.id).update({
                                     username: values.username,
                                     email: values.email
                                 }).then(() => window.location.href = ("/"));
                             }
                             else {
+                                // Create a new user entry in Firebase
                                 firebase.database().ref('/').push({
                                     username: values.username,
                                     email: values.email
                                 }).then(() => window.location.href = ("/"));
                             }
 
+                            // Reset form submitting state
                             setSubmitting(false);
                         }, 400);
                     }}
@@ -110,10 +126,9 @@ class UserForm extends Component {
     }
 }
 
-
-
+// Export a functional component that wraps UserForm and passes route parameters
 export default (props) => (
-    < UserForm
+    <UserForm
         {...props}
         params={useParams()}
     />
