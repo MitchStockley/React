5df1881cd947516ff8402f2eef85a2767620b022
Mitchell <mitchell.stockley@gmail.com>
2023-10-02 11:54:32 +0200

    code commented for userJs

--- Classwork/Chapter9/src/User.js
+++ Classwork/Chapter9/src/User.js
@@ -1,25 +1,30 @@
+// Import necessary modules and components
 import React, { Component } from 'react';
-import firebase from 'firebase/compat/app';
+import firebase from 'firebase/compat/app'; // Firebase for database operations
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-import { Table, Button, Modal } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import {Nav, NavLink} from 'react-bootstrap';
+import { Table, Button, Modal } from 'react-bootstrap'; // Bootstrap components for styling
+import { Link } from 'react-router-dom'; // Link for routing
+import { Nav, NavLink } from 'react-bootstrap'; // Bootstrap navigation components
 
+// Define a class-based component called User
 class User extends Component {
 
+    // Constructor for the component
     constructor(props) {
         super(props);
         this.state = {
-            users: [],
-            showDeleteDialog: false,
-            selectedUser: {}
+            users: [], // Array to store user data
+            showDeleteDialog: false, // Flag to show/hide delete confirmation dialog
+            selectedUser: {} // Selected user for deletion
         };
 
+        // Bind event handlers to this instance
         this.closeDeleteDialog = this.closeDeleteDialog.bind(this);
         this.delete = this.delete.bind(this);
     }
 
+    // Method to delete a user
     delete(e) {
         firebase.database().ref('/' + this.state.selectedUser.key).remove()
             .then(x => {
@@ -32,6 +37,7 @@ class User extends Component {
             });
     }
 
+    // Method to open the delete confirmation dialog
     openDeleteDialog(user) {
         this.setState({
             showDeleteDialog: true,
@@ -39,6 +45,7 @@ class User extends Component {
         });
     }
 
+    // Method to close the delete confirmation dialog
     closeDeleteDialog() {
         this.setState({
             showDeleteDialog: false,
@@ -46,22 +53,29 @@ class User extends Component {
         });
     }
 
+    // Lifecycle method that runs after the component is mounted
     componentDidMount() {
         firebase.database().ref('/')
             .on('value', snapshot => {
                 let returnArr = [];
+
+                // Iterate through Firebase snapshot data and convert to an array
                 snapshot.forEach(data => {
                     var user = data.val();
                     user['key'] = data.key;
                     returnArr.push(user);
                 });
+
+                // Update component state with the retrieved user data
                 this.setState({
                     users: returnArr
                 })
             });
     }
 
+    // Render method for the component
     render() {
+        // Map user data to table rows
         const listUsers = this.state.users.map((user) =>
             <tr key={user.key}>
                 <td>{user.username}</td>
@@ -76,12 +90,15 @@ class User extends Component {
                 </td>
             </tr>
         );
+
+        // Render the component's content
         return (
             <div>
                 <Link to="/add">
                     <Button variant="primary">Add</Button>
                 </Link>
 
+                {/* Display user data in a table */}
                 <Table striped bordered hover>
                     <thead>
                         <tr>
@@ -94,6 +111,7 @@ class User extends Component {
                     <tbody>
                         {listUsers}
                     </tbody>
+                    {/* Modal for user deletion confirmation */}
                     <Modal show={this.state.showDeleteDialog} onHide={this.closeDeleteDialog}>
                         <Modal.Header closeButton>
                             <Modal.Title>Delete User</Modal.Title>
@@ -112,4 +130,6 @@ class User extends Component {
         );
     }
 }
+
+// Export the User component
 export default User;
